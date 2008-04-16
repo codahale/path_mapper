ce09643fb7383b4563bff34008578bb254056330
Coda Hale <coda.hale@gmail.com>
2008-04-16 00:07:38 -0700

    Added path_mapper.routes.parse_path().

--- lib/path_mapper/routes/__init__.py
+++ lib/path_mapper/routes/__init__.py
@@ -4,3 +4,9 @@
 from abstract_route import AbstractRoute
 from static_route import StaticRoute
 
+def parse_path(path):
+  """
+    Converts a path string into a tuple of path components.
+  """
+  return tuple([component for component in path.split('/') if len(component) > 0])
+
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,7 +3,17 @@
 import unittest
 from helpers import test
 
-from path_mapper.routes import StaticRoute
+from path_mapper.routes import parse_path, StaticRoute
+
+class ParsingTests(unittest.TestCase):
+  
+  @test
+  def should_turn_paths_into_tuples(self):
+    self.assertEqual(('home', 'index', 'weeble'), parse_path('/home/index/weeble'))
+    self.assertEqual(('home', 'index', 'weeble'), parse_path('/home/index/weeble/'))
+    self.assertEqual(('home', 'index', 'weeble'), parse_path('home/index/weeble'))
+    self.assertEqual(('home', 'index', 'weeble'), parse_path('home/index/weeble/'))
+  
 
 class StaticRouteTests(unittest.TestCase):
   
