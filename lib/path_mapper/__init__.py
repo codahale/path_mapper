430049c82762141a9de01376d7a95639ceb6ca59
Coda Hale <coda.hale@gmail.com>
2008-04-16 02:07:49 -0700

    Brought PathMapper up to date with the recent changes.

--- lib/path_mapper/__init__.py
+++ lib/path_mapper/__init__.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-from errors import NotFoundError
+from containers import RouteDict
+from errors     import NotFoundError
+from routes     import make_route, StaticRoute
 
 class PathMapper(object):
   def __init__(self):
     super(PathMapper, self).__init__()
-    self.static_patterns = dict()
+    self.static_patterns = RouteDict()
   
-  def connect(self, pattern, options):
-    self.static_patterns[pattern] = options
+  def connect(self, path, name=None, options=None):
+    route = make_route(path, name=name, options=options)
+    if route.static:
+      self.static_patterns.add(route)
   
   def parse(self, path):
-    result = self.static_patterns.get(path, None)
+    result = self.static_patterns.match(path)
     if result:
-      return result
+      return result.options
     else:
       raise NotFoundError('%s not found' % path)
   
--- tests/test_path_mapper.py
+++ tests/test_path_mapper.py
@@ -3,12 +3,13 @@
 import unittest
 from helpers import test
 
-from path_mapper import *
+from path_mapper import PathMapper
+from path_mapper.errors import NotFoundError
 
 class PathMapperTests(unittest.TestCase):
   def setUp(self):
     self.path_mapper = PathMapper()
-    self.path_mapper.connect('/home', { 'controller': 'home', 'action': 'weebles' })
+    self.path_mapper.connect('/home', options={ 'controller': 'home', 'action': 'weebles' })
   
   @test
   def should_parse_a_static_url(self):
