32358ac53d77e6b9c6fb9e478d44d9e6f8cd5ed2
Coda Hale <coda.hale@gmail.com>
2008-04-16 00:15:51 -0700

    Added RouteTree documentation.

--- lib/path_mapper/containers/route_tree.py
+++ lib/path_mapper/containers/route_tree.py
@@ -6,15 +6,25 @@ from prefix_tree import PrefixTree
 from route_list import RouteList
 
 class RouteTree(PrefixTree):
+  """
+    A prefix tree for routes. O(m) matching for paths with m components (e.g.,
+    for '/1/2/3', m = 3).
+  """
+  
   def __init__(self, wildcard='?'):
     super(ListTree, self).__init__(wildcard=wildcard)
   
   def add(self, route):
-    # TODO add a real check here -- does paths() make any sense?
+    """
+      Adds route to the tree for each path in route.paths().
+    """
     for path in route.paths():
       self.setdefault(parse_path(path), RouteList()).add(route)
   
   def match(self, path):
+    """
+      Returns the first route matching path. Returns None if no matches were
+      found.
+    """
     return self.get(parse_path(path), RouteList()).match(path)
   
-
