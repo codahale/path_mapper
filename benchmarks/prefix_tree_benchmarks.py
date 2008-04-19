7fb0eb5cacb42cd44b8a92fae97593178d496e2c
Coda Hale <coda.hale@gmail.com>
2008-04-19 02:43:37 -0700

    Moved old, crappy benchmarks to the new hotness.

--- benchmarks/prefix_tree_benchmarks.py
+++ benchmarks/prefix_tree_benchmarks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 import random, string, sys
-sys.path.insert(0, 'lib')
+from helpers import benchmark, run_benchmarks
 from path_mapper.containers import PrefixTree
 
 random.seed = 0xDEADBEEF
@@ -9,6 +9,7 @@ random_values = list()
 for i in xrange(1, 500):  
   random_values.append(''.join([random.choice(string.letters + string.digits) for i in xrange(random.randint(1, 30))]))
 
+@benchmark(100)
 def create_tree():
   tree = PrefixTree()
   for value in random_values:
@@ -16,17 +17,11 @@ def create_tree():
   return tree
 
 random_tree = create_tree()
+random_value = random.choice(random_values)
 
+@benchmark(100000)
 def find_in_tree():
-  return 'blah' in random_tree
+  return random_value in random_tree
 
-if __name__=='__main__':
-  from timeit import Timer
-  
-  print "Creating a tree with 500 elements..."
-  t = Timer("create_tree()", "from __main__ import create_tree")
-  print '200 loops, 3 times each: %f/%f/%f (s)' % tuple(t.repeat(number=200))
-  
-  print "Finding an element in a tree of 500 elements"
-  t = Timer("find_in_tree()", "from __main__ import find_in_tree")
-  print '%f (ms)' % (t.timeit() / 1000.0)
+if __name__ == '__main__':
+  run_benchmarks()
